ab6888da30010eba159af67d30b7aeb458cd8f1c
rikutech <rikutech@gmail.com>
2018-11-05 11:31:20 +0900

    OK

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,7 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
-config :hound, browser: "firefox"
+config :hound, driver: "chrome_driver", browser: "chrome_headless"
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
--- lib/extagram/auto_like.ex
+++ lib/extagram/auto_like.ex
@@ -5,10 +5,22 @@ defmodule Extagram.AutoLike do
   use Hound.Helpers
 
   def start do
-    hound_session()
+    Hound.start_session()
     navigate_to("http://instagram.com")
+    login()
+    start_like()
+    #Hound.end_session
+  end
+
+  defp login do
     fill_field({:name, "username"}, System.get_env("INSTAGRAM_USERNAME"))
     fill_field({:name, "password"}, System.get_env("INSTAGRAM_PASSWORD"))
-    submit_element({:type, "submit"})
+    submit_element({:xpath, "//button[@type='submit']"})
+    click({:xpath, "//button[contains(text(), '後で')]"})
+  end
+
+  defp start_like do
+    navigate_to("https://www.instagram.com/everytraveler/")
+    click({:xpath, "//a[@href='/everytraveler/followers/']"})
   end
 end
